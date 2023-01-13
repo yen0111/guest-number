9d0c680cb9ec32cf5fb147ea260b93c98df99f15
Yen <bird2001719@gmail.com>
2023-01-13 10:50:49 +0800

    First

---
+++ guess.py
@@ -0,0 +1,22 @@
+#產生一個隨機整數1~100(不要印出來)
+#讓使用者重複輸入數字去猜
+#猜對的話 印出"終於猜對了!"
+#猜錯的話 要告訴他 比答案大/小
+
+import random
+
+r = random.randint (1, 100)
+count = 0
+while True:
+	count += 1
+	num = input("請猜數字: ")
+	num = int(num)
+	if num == r:
+		print("你猜中了")
+		print("這是你猜的第", count, "次")
+		break
+	elif num > r:
+		print("太大了")
+	elif num < r:
+		print("太小了")
+	print("這是你猜的第", count, "次")
